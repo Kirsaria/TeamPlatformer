ff26924d5e2afcf44a878eb7ebea305201c667ba
sooyae8 <ssoyea8@gmail.com>
2024-10-03 00:09:48 +0300

    Добавление прыжка на клавишу W

--- TeamPlatformer/Assets/Scripts/Player/PlayerController.cs
+++ TeamPlatformer/Assets/Scripts/Player/PlayerController.cs
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour
         float movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.005f)
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))&& Mathf.Abs(rb.velocity.y) < 0.005f)
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
