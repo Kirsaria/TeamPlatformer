e5b312e7c4acf320a1cc0eed7decdcb87152ecf0
sooyae8 <ssoyea8@gmail.com>
2024-10-03 00:01:09 +0300

    Исправление гравитации и прыжка

--- TeamPlatformer/Assets/Scripts/Player/PlayerController.cs
+++ TeamPlatformer/Assets/Scripts/Player/PlayerController.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 1f;
+    public float speed;
+    public float jumpForce;
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
 
     void Update()
     {
         float movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime; 
+        transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime;
 
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.005f)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+
+        sr.flipX = movement < 0 ? true : false;
 
     }
 
