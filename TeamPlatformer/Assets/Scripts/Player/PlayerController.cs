5bf199b19ce5e882fab8fca8a3a397702737316d
Kirsa <vicronika27@gmail.com>
2024-10-03 23:30:10 +0300

    Исправление анимации

--- TeamPlatformer/Assets/Scripts/Player/PlayerController.cs
+++ TeamPlatformer/Assets/Scripts/Player/PlayerController.cs
@@ -31,16 +31,7 @@ public class PlayerController : MonoBehaviour
         float movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime;
         animator.SetFloat("HorizontalMove", Mathf.Abs(movement));
-        if (Mathf.Abs(movement) > 0.1f)
-        {
-            box.size = Vector2.Lerp(box.size, new Vector2(0.1583978f, 0.2341504f), Time.deltaTime * lerpSpeed);
-            box.offset = Vector2.Lerp(box.offset, new Vector2(-0.001899615f, -0.03033152f), Time.deltaTime * lerpSpeed);
-        }
-        else
-        {
-            box.size = Vector2.Lerp(box.size, originalSize, Time.deltaTime * lerpSpeed);
-            box.offset = Vector2.Lerp(box.offset, originalOffset, Time.deltaTime * lerpSpeed);
-        }
+       
 
         if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))&& Mathf.Abs(rb.velocity.y) < 0.005f)
         {
