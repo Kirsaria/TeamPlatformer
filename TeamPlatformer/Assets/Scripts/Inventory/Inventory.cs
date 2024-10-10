0d243b83fd78176227c781a291a2fd4a572d4393
Kirsa <vicronika27@gmail.com>
2024-10-11 01:32:43 +0300

    Закрытие инвентаря

--- TeamPlatformer/Assets/Scripts/Inventory/Inventory.cs
+++ TeamPlatformer/Assets/Scripts/Inventory/Inventory.cs
@@ -6,6 +6,26 @@ public class Inventory : MonoBehaviour
 {
     public bool[] isFull;
     public GameObject[] slots;
+    public GameObject inventory;
     public int starCount = 0;
     public int starSlotIndex = -1;
+    private bool inventoryOn;
+
+    private void Start()
+    {
+        inventoryOn = false;
+    }
+    public void Bag()
+    {
+        if(inventoryOn == false)
+        {
+            inventoryOn = true;
+            inventory.SetActive(true);
+        }
+        else if(inventoryOn == true)
+        {
+            inventoryOn = false;
+            inventory.SetActive(false);
+        }
+    }
 }
