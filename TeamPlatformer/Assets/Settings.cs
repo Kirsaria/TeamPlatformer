b4371d5116fbc9ac29fa4d03f63d74309be383fd
sooyae8 <ssoyea8@gmail.com>
2024-10-16 00:42:18 +0300

    меню

---
+++ TeamPlatformer/Assets/Settings.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRate + "Hz";
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.ClearOptions();
+        resolutionDropdown.AddOptions(options);
+        LoadSettings(currentResolutionIndex);
+        resolutionDropdown.onValueChanged.AddListener(delegate {
+            SetResolution(resolutionDropdown.value);
+        });
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        SaveSettings();
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value);
+    }
+
+    public void LoadSettings(int resolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("ResolutionPreference"))
+        {
+            resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
+        }
+        else
+        {
+            resolutionDropdown.value = resolutionIndex;
+        }
+    }
+}
