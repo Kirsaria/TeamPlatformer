3524492a40c4603478fb8e3055cb747d4aed6e88
sooyae8 <ssoyea8@gmail.com>
2024-10-19 23:35:39 +0300

    Дверь

---
+++ TeamPlatformer/Assets/CheckingtheNumberofStars.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckingtheNumberofStars : MonoBehaviour
+{
+    public int requiredStarCount = 5;
+    public Sprite doorOpenSprite;
+    public Sprite doorClosedSprite;
+
+    private SpriteRenderer spriteRenderer;
+    public Text starCountText;
+    public Text message;
+    private Collider2D doorCollider;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = doorClosedSprite;
+        doorCollider = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            int currentStarCount = GetCurrentStarCount();
+            if (spriteRenderer.sprite != doorOpenSprite)
+            {
+                if (currentStarCount >= requiredStarCount)
+                {
+                    OpenDoor();
+                }
+                else
+                {
+                    message.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        message.gameObject.SetActive(false);
+    }
+
+    private int GetCurrentStarCount()
+    {
+        if (starCountText != null)
+        {
+
+            if (int.TryParse(starCountText.text, out int starCount))
+            {
+                return starCount;
+            }
+        }
+        return 0;
+    }
+
+
+    private void OpenDoor()
+    {
+        spriteRenderer.sprite = doorOpenSprite;
+        if (doorCollider != null)
+        {
+            doorCollider.enabled = false;
+        }
+    }
+}
